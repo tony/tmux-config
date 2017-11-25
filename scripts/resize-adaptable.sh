df09807d564c55c0eec846f5bb7387f8295537e3
Tony Narlock <tony@git-pull.com>
2017-11-24 22:11:24 -0600

    error handling for percentage and layout name

--- scripts/resize-adaptable.sh
+++ scripts/resize-adaptable.sh
@@ -21,9 +21,18 @@ if [ ! -z "$tflag" ]; then
 fi
 if [ ! -z "$pflag" ]; then
     printf "Option -p specified: %s\n" "$percentage"
+    intre='^[0-9]+$'
+    if ! [[ $percentage =~ $intre ]] ; then
+        printf "Percentage (-p) must be an integer" >&2
+        exit 1
+    fi
 fi
 if [ ! -z "$lflag" ]; then
     printf "Option -l specified: %s\n" "$layout_name"
+    if [[ $layout_name != 'main-horizontal' ]] && [[ $layout_name != 'main-vertical' ]] ; then
+        printf "layout name must be main-horizontal or main-vertical" >&2
+        exit 1
+    fi
 fi
 
 shift $(($OPTIND - 1))

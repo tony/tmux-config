9cf16db8f5a95a5c0462f8fe7611c18a0c0d4cb9
Tony Narlock <tony@git-pull.com>
2017-11-24 23:38:55 -0600

    run this through freebsd /bin/sh and fix stuff

--- scripts/resize-adaptable.sh
+++ scripts/resize-adaptable.sh
@@ -50,31 +50,30 @@ do
 done
 
 if [ ! -z "$pflag" ]; then
-    intre='^[0-9]+$'
-    if ! [[ $percentage =~ $intre ]] ; then
+    if ! [ "$percentage" -eq "$percentage" ] 2> /dev/null; then
         printf "Percentage (-p) must be an integer" >&2
         exit 1
     fi
 fi
 if [ ! -z "$lflag" ]; then
-    if [[ $layout_name != 'main-horizontal' ]] && [[ $layout_name != 'main-vertical' ]] ; then
+    if [ $layout_name != 'main-horizontal' ] && [ $layout_name != 'main-vertical' ] ; then
         printf "layout name must be main-horizontal or main-vertical" >&2
         exit 1
     fi
 fi
 
-if [[ $layout_name == 'main-vertical' ]]; then
+if [ "$layout_name" = "main-vertical" ]; then
     MAIN_PANE_SIZE=$(expr $(tmux display -p '#{window_width}') \* $percentage \/ 100)
     MAIN_SIZE_OPTION='main-pane-width'
 
 fi
 
-if [[ $layout_name == 'main-horizontal' ]]; then
+if [ "$layout_name" = "main-horizontal" ]; then
     MAIN_PANE_SIZE=$(expr $(tmux display -p '#{window_height}') \* $percentage \/ 100)
     MAIN_SIZE_OPTION='main-pane-height'
 fi
 
-if [[ ! -z "$target" ]]; then
+if [ ! -z "$target" ]; then
     tmux setw -t $target $MAIN_SIZE_OPTION $MAIN_PANE_SIZE; tmux select-layout -t $target $layout_name
 else
     tmux setw $MAIN_SIZE_OPTION $MAIN_PANE_SIZE; tmux select-layout $layout_name
